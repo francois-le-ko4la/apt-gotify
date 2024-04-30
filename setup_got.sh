843a943ff536deaedc29e29787ba15a20ccf5828
Ko4la <francois@le.ko4la.fr>
2024-04-30 21:31:32 +0200

    Update setup_got.sh

--- setup_got.sh
+++ setup_got.sh
@@ -12,15 +12,25 @@ SCRIPT_PATH="/opt/scripts"
 URL="https://raw.githubusercontent.com/francois-le-ko4la/rasp-gotify/main"
 PKG="python3-full"
 LIB="requests python-dotenv"
+ERROR="ERROR"
+INFO="INFO"
 
 # Logging function
 log() {
-    echo "$(date --iso-8601=seconds) - GOT - $1"
+    local SEV="$1"
+    local MSG="$2"
+    echo "$(date --iso-8601=seconds) - GOT - $SEV - $MSG"
 }
+
+# Check the user
+if [ "${EUID:-0}" -ne 0 ] || [ "$(id -u)" -ne 0 ]; then
+    log "$ERROR" "Please run this script as root or using sudo!"
+    exit 1
+fi
 
 # Check if the platform is Linux
 if [ "$(uname)" != "Linux" ]; then
-    log "This script only works on Linux systems."
+    log "$ERROR" "This script only works on Linux systems."
     exit 1
 fi
 
@@ -28,61 +38,61 @@ fi
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     if [ "$ID" = "ubuntu" ] && [ "${VERSION_ID%.*}" -ge 20 ]; then
-        log "Ubuntu 20.04 or newer detected."
+        log "$INFO" "Ubuntu 20.04 or newer detected."
     else
-        log "Unsupported Ubuntu version. Exiting..."
+        log "$ERROR" "Unsupported Ubuntu version. Exiting..."
         exit 1
     fi
 else
-    log "Unable to detect the operating system."
+    log "$ERROR" "Unable to detect the operating system."
     exit 1
 fi
 
-log "Installing python3-full..."
+log "$INFO" "Installing python3-full..."
 sudo apt-get -yq install $PKG > /dev/null 2>&1 || { log "Installation of python3-full failed."; exit 1; }
 
-log "Creating scripts repository..."
+log "$INFO" "Creating scripts repository..."
 mkdir -p $SCRIPT_PATH
-log "Creating python venv..."
+log "$INFO" "Creating python venv..."
 python3 -m venv $SCRIPT_PATH/venv > /dev/null 2>&1 || { log "Creation of python venv failed."; exit 1; }
-log "Installing libraries..."
+log "$INFO" "Installing libraries..."
 $SCRIPT_PATH/venv/bin/python -m pip install --upgrade pip > /dev/null 2>&1
 $SCRIPT_PATH/venv/bin/python -m pip install $LIB > /dev/null 2>&1 || { log "Installation of libraries failed."; exit 1; }
-log "Downloading Gotify files..."
+log "$INFO" "Downloading Gotify files..."
 for file in apt_gotify.py notification.py ssh_gotify.py; do
     wget -q -O $SCRIPT_PATH/$file $URL/$file > /dev/null 2>&1 || { log "Download of $file failed."; exit 1; }
     chmod +x $SCRIPT_PATH/$file
     if [ -f "/etc/update-motd.d/$file" ]; then
-        log "File $file exists. Deleting $file."
+        log "$INFO" "File $file exists. Deleting $file."
         rm /etc/update-motd.d/$file
     fi
 done
 
 #.env file
 if [ -f "$SCRIPT_PATH/.env" ]; then
-    log "File $SCRIPT_PATH/.env exists. Edit your parameters!"
+    log "$INFO" "File $SCRIPT_PATH/.env exists. Edit your parameters!"
 else
-    log "File $SCRIPT_PATH/.env does not exist. Create a file. Edit your parameters!"
+    log "$INFO" "File $SCRIPT_PATH/.env does not exist. Create a file. Edit your parameters!"
     wget -q -O $SCRIPT_PATH/.env $URL/env_example > /dev/null 2>&1 || { log "Download of .env failed."; exit 1; }
 fi
 
 # Setup apt notification
-log "Adding apt notification..."
+log "$INFO" "Adding apt notification..."
 wget -q -O /etc/apt/apt.conf.d/99update-notifier-gotify $URL/99update-notifier-gotify > /dev/null 2>&1 || { log "Download of apt.conf.d/99update-notifier-gotify failed."; exit 1; }
 
 # Set up SSH notification
 if grep -q "/opt/scripts/ssh_gotify.py" /etc/pam.d/sshd; then
-    log "SSH Notification already set up in /etc/pam.d/sshd."
+    log "$INFO" "SSH Notification already set up in /etc/pam.d/sshd."
 else
     echo "# Notify SSH" | sudo tee -a /etc/pam.d/sshd > /dev/null
     echo "session optional pam_exec.so /opt/scripts/ssh_gotify.py" | sudo tee -a /etc/pam.d/sshd > /dev/null 2>&1 || { log "Setting up SSH notification failed."; exit 1; }
-    log "SSH notification has been added to /etc/pam.d/sshd."
+    log "$INFO" "SSH notification has been added to /etc/pam.d/sshd."
 fi
 
 # reboot_gotify.service
-log "Adding reboot notification..."
+log "$INFO" "Adding reboot notification..."
 wget -q -O /etc/systemd/system/reboot_gotify.service $URL/reboot_gotify.service > /dev/null 2>&1 || { log "Download of reboot_gotify.service failed."; exit 1; }
 sudo systemctl enable reboot_gotify.service
 sudo systemctl start reboot_gotify.service
 
-log "Operation completed successfully."
+log "$INFO" "Operation completed successfully."
